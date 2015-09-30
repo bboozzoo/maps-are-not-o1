2abbbd6d22796b6b9239fd007d269df58bd94538
connor4312 <connor@peet.io>
2015-09-30 16:41:52 -0400

    Fix incorrect axis label

--- plot.r
+++ plot.r
@@ -7,7 +7,7 @@ png(filename="plot.png",
     height=480)
 
 plot(data$Elements, data$Map/data$Elements, type=c("b"), log=c("x"), ylim=c(20,160),
-        xlab="Number of Elements", ylab="Search Time (s)", col="blue")
+        xlab="Number of Elements", ylab="Search Time (ns)", col="blue")
 title(main="Lookup Time for Keys in a Hashmap vs. Binary Search on a List")
 lines(data$Elements, y=data$Slice/data$Elements, col="red", type="b")
 legend(10^2, y=160, c("Slice", "Map"), col=c("red", "blue"), lwd=c(2.5, 2.5), lty=c(1, 1))
